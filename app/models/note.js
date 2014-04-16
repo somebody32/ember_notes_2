f92ef747781f4571b7405025d2e9e8d2cf6beb52
Ilya Zayats <somebody32@gmail.com>
2014-04-16 21:43:16 +0400

    first commit

---
+++ app/models/note.js
@@ -0,0 +1,29 @@
+var Note = DS.Model.extend({
+  updated: DS.attr('date'),
+  text: DS.attr('string')
+});
+
+Note.reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      updated: new Date(13526545200000),
+      text: "blabla note1"
+
+    },
+    {
+      id: 2,
+      updated: new Date(13326545200000),
+      text: "blabla note2"
+
+    },
+    {
+      id: 3,
+      updated: new Date(13576545200000),
+      text: "blabla note3"
+
+    }
+  ]
+})
+
+export default Note;
